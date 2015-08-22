f00272e24db033eabcb3ce97122312cd86a495be
Ashutosh Das <git.pyprism@gmail.com>
2015-08-22 22:31:19 +0600

    add logout, login routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,14 @@ var express = require('express'),
 var routes = function(Account) {
     var router = express.Router();
 
+    // routes for registration
     router.route('/register')
         .get(function(req, res) {
             res.render('pages/register', { error: null});
         })
         .post(function(req, res) {
             Account.count({}, function( err, count){
-                if (count == 1){
+                if (count == 1){   //only single account permitted to register
                     return res.render('pages/register', {error: "There is already an account existed!"});
                 } else {
                     Account.register(new Account({username: req.body.email}), req.body.password, function(err, account) {
@@ -22,13 +23,29 @@ var routes = function(Account) {
                         }
 
                         passport.authenticate('local')(req, res, function () {
-                            res.redirect('/');
+                            res.redirect('/dashboard');
                         });
 
                     });
                 }
             });
 
+        });
+
+    //routes for login
+    router.route('/login')
+        .get(function(req, res) {
+            res.render('pages/login', { error: null});
+        })
+        .post( passport.authenticate('local'), function(req, res) {
+            res.redirect('/dashboard');
+        });
+
+    //logout route
+    router.route('/logout')
+        .get(function(req, res){
+           req.logout();
+            res.redirect('/');
         });
     return router;
 };
