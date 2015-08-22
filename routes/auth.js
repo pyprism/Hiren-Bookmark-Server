8a95df6225c126149da8bf171559d5bad764a6b1
Ashutosh Das <git.pyprism@gmail.com>
2015-08-22 08:57:37 +0600

    add only single account login permission

--- routes/auth.js
+++ routes/auth.js
@@ -9,19 +9,26 @@ var routes = function(Account) {
 
     router.route('/register')
         .get(function(req, res) {
-            res.render('pages/register');
+            res.render('pages/register', { error: null});
         })
         .post(function(req, res) {
-            Account.register(new Account({username: req.body.username}), req.body.password, function(err, account) {
-                if (err) {
-                    return res.render('pages/register', { account : account });
+            Account.count({}, function( err, count){
+                if (count == 1){
+                    return res.render('pages/register', {error: "There is already an account existed!"});
+                } else {
+                    Account.register(new Account({username: req.body.email}), req.body.password, function(err, account) {
+                        if (err) {
+                            return res.render('pages/register', { error : err });
+                        }
+
+                        passport.authenticate('local')(req, res, function () {
+                            res.redirect('/');
+                        });
+
+                    });
                 }
-
-                passport.authenticate('local')(req, res, function () {
-                    res.redirect('/');
-                });
-
             });
+
         });
     return router;
 };
