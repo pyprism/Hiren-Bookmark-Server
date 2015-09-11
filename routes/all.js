41d7518cf94cb4a71bff0dc6f6045958f64d850c
Ashutosh Das <git.pyprism@gmail.com>
2015-09-11 21:06:18 +0600

    fix waterfall array

--- routes/all.js
+++ routes/all.js
@@ -1,7 +1,8 @@
 /**
  * Created by prism on 8/31/15.
  */
-var express = require('express');
+var express = require('express'),
+    async = require('async');
 
 var routes = function(Tag, URL) {
     var router = express.Router();
@@ -12,28 +13,44 @@ var routes = function(Tag, URL) {
     router.route('/')
         .get(function(req, res){
            var pageNo = undefined === req.params.pageNo ? 1 : req.params.pageNo;
-            //console.log(req.params.pageNo);
-            //console.log(req.params.pageNo);
+            //----------------------
             URL.paginate({}, {
                 page: pageNo, limit: 10
                 //page: req.params.pageNo, limit: 10
-            }, function(err, result) {
-                //console.log(result);
+            }, function (err, result) {
+               // console.log(result);
                 var data = [];
                 data.row = [];
                 data.total = result.length;
-                result.forEach(function(element, index, array) {
+                result.forEach(function (element) {
+                    async.waterfall([
+                       function (callback) {
+                           console.log(element._id);
+                            Tag.findOne({ref: element._id}, function (err, result) {
+                                //console.log(result)
+                                data.row.push({'title': element.title, 'tag': result.name});
+                            });
+                            callback(null, data);
+                    }
+                    ], function (err, result) {
+                        if(!err)
+                            console.log(result);
+                    });
+                });
+
+                /*result.forEach(function(element, index, array) {
                     Tag.findOne({ref: element._id}, function(err, result) {
                       console.log(result.name);
                         data.row.push({'title': element.title, 'tag': result.name});
                         console.log(data);
                     });
-                });
+                });*/
 
                 //console.log(result.length);
             });
             res.render('pages/all');
         });
+
 
     return router;
 };
