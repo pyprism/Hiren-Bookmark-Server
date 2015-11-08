8f08c664b596a155739909e6604b01f4b3559110
Ashutosh Das <git.pyprism@gmail.com>
2015-11-08 20:13:25 +0600

    Add new route

--- routes/ajax.js
+++ routes/ajax.js
@@ -5,12 +5,12 @@ var express = require('express'),
     request = require('request'),
     cheerio = require('cheerio');
 
-var routes = function(Tag) {
+var routes = function (Tag) {
     var router = express.Router();
 
     //route for fetching title using AJAX
     router.route('/title')
-        .post(function(req, res) {
+        .post(function (req, res) {
             request(req.body.hiren, function(error, response, html) {
                 if(!error && response.statusCode == 200) {
                     var $ = cheerio.load(html);
@@ -22,12 +22,25 @@ var routes = function(Tag) {
             });
         });
 
-    //get all tags name
+    //route for tag cloud
     router.route('/tags')
-        .get(function(req, res) {
-            Tag.find({}, 'name', function(err, data) {
+        .get(function (req, res) {
+/*            Tag.find({}, 'name', function(err, data) {
                 if(!err)
                     res.send(data);
+            });*/
+            Tag.findAsync({}, {name: 1, urlId: 1, _id: 0}).then(function(tags) {
+                var result = [];
+                tags.forEach(function(tag) {
+                    var tempResult = {};
+                    tempResult.text = tag.name;
+                    tempResult.weight = (tag.urlId).length;
+                    tempResult.link = '/dashboard/tags/' + tag.name;
+                    result.push(tempResult);
+                });
+                res.send(result);
+            }).catch(function(err) {
+                res.status(500).send(err);
             });
         });
 
