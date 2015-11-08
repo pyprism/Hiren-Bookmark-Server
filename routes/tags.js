1dd0dd80887ee771fb1fa072a1cb555d69191370
Ashutosh Das <git.pyprism@gmail.com>
2015-11-08 20:13:43 +0600

    add new route

--- routes/tags.js
+++ routes/tags.js
@@ -2,17 +2,35 @@
  * Created by prism on 8/30/15.
  */
 var express = require('express');
+var async = require('async');
 
-var routes = function(Tag) {
+var routes = function(Tag, Urls) {
     var router = express.Router();
 
-    /*
-    List all tags
-     */
+    //serve tag page
     router.route('/')
-        .get(function(req, res){
-
+        .get(function (req, res){
+            res.render('pages/tags');
         });
+
+    //get specific tag details
+    router.route('/:name')
+        .get(function (req, res) {
+                var values = [];
+                 Tag.findOneAsync({'name': req.params.name}, {urlId: 1, _id: 0}).then(function (data) {
+                    data['urlId'].forEach(function (urlId) {
+                        Urls.findByIdAsync(urlId).then(function (result) {
+                            values.push(result);
+                        }).catch(function (err) {
+                            res.status(500).send(err);
+                        });
+                    });
+                }).catch(function (err) {
+                    res.status(500).send(err);
+                });
+
+                res.send(values);
+	 });
 
     return router;
 };
