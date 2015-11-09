3b09b38398e54fa53b17724f3e0b2d0f27db9c03
Ashutosh Das <git.pyprism@gmail.com>
2015-11-09 16:50:34 +0600

    fix table structure

--- public/js/table.js
+++ public/js/table.js
@@ -1,157 +1,28 @@
 /**
  * Created by prism on 9/1/15.
  */
-$('#table').bootstrapTable({
-    sortName: 'title',
-    pagination: true,
-    search: true,
-    columns: [{
-        field: 'title',
-        title: 'Title',
-        sortable: true,
-        formatter: function(value, row, index, href){
-            return '<a href="' + href + '" target="_blank"">' + value + '</a>';
-        }
-    }, {
-        field: 'tag',
-        title: 'Tag',
-        sortable: true
-    }, {
-        field: 'date',
-        title: 'Added on',
-        sortable: true
-    }],
-    data: [{
-        title: "Facebook.com",
-        tag: 'anjum',
-        date: '20/9/2013'
-    }, {
-        title: "Facebook.net",
-        tag: 'nisha',
-        date: '20/5/2013'
-    }, {
-        title: "Facebook.com",
-        tag: 'naima',
-        date: '20/2/2013'
-    },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },{
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        },
-        {
-            title: "Facebook.com",
-            tag: 'naima',
-            date: '20/2/2013'
-        }
-    ]
+$( document ).ready(function() {
+    $('#table').bootstrapTable({
+        url: '/dashboard/list/all',
+        sortName: 'title',
+        pagination: true,
+        search: true,
+        columns: [{
+            field: 'title',
+            title: 'Title',
+            sortable: true,
+            formatter: function(value, row, index){
+                return '<a href="' + row.href + '" target="_blank"">' + value + '</a>';
+            }
+        }, {
+            field: 'tag',
+            title: 'Tag',
+            sortable: true
+        }, {
+            field: 'date',
+            title: 'Added on',
+            sortable: true
+        }],
+
+    });
 });
