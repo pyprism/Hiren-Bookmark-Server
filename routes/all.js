2644e838a3d4c3add080cfd6d7a56f4345f87169
Ashutosh Das <git.pyprism@gmail.com>
2015-11-09 16:52:18 +0600

    update

--- routes/all.js
+++ routes/all.js
@@ -1,58 +1,41 @@
 /**
  * Created by prism on 8/31/15.
  */
-var express = require('express'),
-    async = require('async');
+var express = require('express');
+var Promise = require("bluebird");
 
-var routes = function(Tag, URL) {
+var routes = function(Tag, Url) {
     var router = express.Router();
 
     /*
-     List all tags
+     just serve the page
      */
     router.route('/')
-        .get(function(req, res){
-           var pageNo = undefined === req.params.pageNo ? 1 : req.params.pageNo;
-            //----------------------
-            URL.paginate({}, {
-                page: pageNo, limit: 10
-                //page: req.params.pageNo, limit: 10
-            }, function (err, result) {
-               // console.log(result);
-                var data = [];
-                data.row = [];
-                data.total = result.length;
-                /*result.forEach(function (element) {
-                    async.waterfall([
-                       function (callback) {
-                            Tag.findOne({urlId: { $in : [element._id]} }, function (err, tagResult) {
-                                //console.log(err);
-                               // console.log(result);
-                               data.row.push({'title': element.title, 'tag': tagResult.name});
-                               callback(null, data);
-                               //callback(null, tagResult);
-                            });
-                            //callback(null, data);
-                    }
-                    ], function (err, hiren) {
-                        if(!err)
-                           console.log(hiren);
-                    });
-                });*/
-
-                /*result.forEach(function(element, index, array) {
-                    Tag.findOne({ref: element._id}, function(err, result) {
-                      console.log(result.name);
-                        data.row.push({'title': element.title, 'tag': result.name});
-                        console.log(data);
-                    });
-                });*/
-
-                //console.log(result.length);
-            });
+        .get(function (req, res){
             res.render('pages/all');
         });
 
+    //get all url and associate tag
+    router.route('/all')
+        .get(function (req, res) {
+            var values = [];
+            var obj = {};
+            Url.findAsync().then(function(urls) {
+                return Promise.each(urls, function(url) {
+                    obj['title'] = url['title'];
+                    obj['date'] = url['createdOn'];
+                    obj['href'] = url['href'];
+                    return Tag.findOneAsync({'urlId': url._id}).then(function (hiren) {
+                        obj['tag'] = hiren['name'];
+                        values.push(obj);
+                    });
+                }).then(function () {
+                    res.send(values);
+                }).caught(function (err) {
+                    res.status(500).send(err);
+                });
+            });
+        });
 
     return router;
 };
