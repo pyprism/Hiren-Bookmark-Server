9eeb298d5ed9709f0f5ccc5911301f7737b18e29
Ashutosh Das <git.pyprism@gmail.com>
2015-11-09 16:50:07 +0600

    Add new route

--- routes/tags.js
+++ routes/tags.js
@@ -2,7 +2,7 @@
  * Created by prism on 8/30/15.
  */
 var express = require('express');
-var async = require('async');
+var Promise = require("bluebird");
 
 var routes = function(Tag, Urls) {
     var router = express.Router();
@@ -16,21 +16,37 @@ var routes = function(Tag, Urls) {
     //get specific tag details
     router.route('/:name')
         .get(function (req, res) {
-                var values = [];
-                 Tag.findOneAsync({'name': req.params.name}, {urlId: 1, _id: 0}).then(function (data) {
-                    data['urlId'].forEach(function (urlId) {
-                        Urls.findByIdAsync(urlId).then(function (result) {
-                            values.push(result);
-                        }).catch(function (err) {
-                            res.status(500).send(err);
-                        });
+            Tag.findOneAsync({'name': req.params.name}, {urlId: 1, _id: 0}).then(function (data) {
+                return Promise.map(data['urlId'], function (urlId) {
+                    return Urls.findByIdAsync(urlId).then(function (result) {
+                        return result;
                     });
-                }).catch(function (err) {
+                });
+            })
+                .then(function (values) {
+                    res.render('pages/single_tag', {'values': values, 'tag': req.params.name});
+                    //res.send(values);
+                })
+               .caught(function (err) {
                     res.status(500).send(err);
+               });
+        });
+
+    //Delete url and reference from tag
+    router.route('/:del/:id')
+        .get(function (req, res) {
+            if (req.params.del === 'delete') {
+                Urls.removeAsync({'_id': req.params.id}).then(function() {
+                    Tag.findOneAsync({'name': req.query.tag}).then(function (data) {
+                        var index = data.urlId.indexOf(req.params.id);
+                        data.urlId.splice(index);
+                        data.save();
+                    });
                 });
 
-                res.send(values);
-	 });
+            }
+            res.redirect('/dashboard/tags/');
+        });
 
     return router;
 };
