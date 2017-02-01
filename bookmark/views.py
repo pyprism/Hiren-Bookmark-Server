a4ee1daa11681a40bc739977b6858caf67fd1715
Ashutosh Das <git.pyprism@gmail.com>
2017-02-01 17:16:11 +0600

    added new views

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib import messages
+from .models import Bookmark
 
-# Create your views here.
+
+def login(request):
+    """
+    Handle authentication
+    :param request:
+    :return:
+    """
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
+
+
+@login_required
+def dashboard(request):
+    bookmarks = Bookmark.objects.order_by('-id')
+    return render(request, 'dashboard.html', {'bookmarks': bookmarks})
