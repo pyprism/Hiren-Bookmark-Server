a4a1372dcdc4065d930bffd4c6d55911ca066d87
Ashutosh Das <git.pyprism@gmail.com>
2017-02-02 21:31:36 +0600

    added new url

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from bookmark import views
+from django.contrib.auth import views as auth
 
 urlpatterns = [
-    url(r'^', views.login, name='login'),
-    url(r'dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^$', views.login, name='login'),
+    url(r'^logout/$', auth.logout, {'next_page': '/'}, name="logout"),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
 ]
