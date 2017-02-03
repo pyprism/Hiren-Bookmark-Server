b49f21eab69231fead8ac4655d7f089693496c45
Ashutosh Das <git.pyprism@gmail.com>
2017-02-03 19:06:29 +0600

    added new views

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.contrib import messages
 from .models import Bookmark
+from .forms import BookmarkForm
+from django.http import JsonResponse
+from urllib.request import urlopen
+from .utils import Title
+from django.views.decorators.csrf import csrf_exempt
 
 
 def login(request):
@@ -29,5 +34,32 @@ def login(request):
 
 @login_required
 def dashboard(request):
+    """
+    Returns whole bookmark data
+    :param request:
+    :return:
+    """
     bookmarks = Bookmark.objects.order_by('-id')
     return render(request, 'dashboard.html', {'bookmarks': bookmarks})
+
+
+@login_required
+def form(request):
+    if request.method == 'POST':
+        frm = BookmarkForm(request.POST)
+        if frm.is_valid():
+            frm.save()
+            return JsonResponse(frm, safe=False)
+    return render(request, 'form.html')
+
+
+@login_required
+@csrf_exempt
+def title(request):
+    if request.method == 'POST':
+        # html_string = str(urlopen('url').read())
+        # parser = Title.TitleParser()
+        # parser.feed(html_string)
+        # print(parser.title)
+        print("called")
+        return request.POST.get('title')
