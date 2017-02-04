bf8cb33eb8d0650a0b6b947b554f7be2cf2672fc
Ashutosh Das <git.pyprism@gmail.com>
2017-02-04 14:35:42 +0600

    added new function

--- static/js/hiren.js
+++ static/js/hiren.js
@@ -3,13 +3,71 @@
  */
 
 (function () {
-    $('#url').focusout(function () {
+    $('#url').focusout(function () {  //post to title view
+        $('#title').attr("placeholder", "Loading title..");
         $.ajax({
-            url: 'title/',
+            url: '/title/',
             method: 'POST',
-            data: 'hello'
+            data: {
+                'url': $('#url').val()
+            }
         }).then(function (data) {
-            console.log(data);
+            if(data.title){
+                $('#title').val(data.title);
+            } else {
+                $('#title').attr("placeholder", "Title generation failed! Check the url.");
+            }
         })
     })
 })();
+
+function tag() { //function for selectize (tag input)
+    $.ajax({
+        url: '/tags/'
+    }).then(function (response) {
+        console.log(response);
+        // let nisha = [];
+        // response.data.map(function (hiren) {
+        //     let bunny = {'value': '', 'text': ''};
+        //     bunny['value'] = hiren.name;
+        //     bunny['text'] = hiren.name;
+        //     nisha.push(bunny);
+        // });
+        $('#tags').selectize({
+            delimiter: ';',
+            persist: false,
+            //options: nisha,
+            create: function(input) {
+                return {
+                    value: input,
+                    text: input
+                }
+            }
+        });
+    });
+}
+
+function encrypt(text, key, iv) {
+    var cipher = forge.cipher.createCipher('AES-CBC', key);
+    cipher.start({iv: iv});
+    cipher.update(forge.util.createBuffer(text));
+    cipher.finish();
+    var encrypted = cipher.output;
+    return encrypted.toHex();
+}
+
+function decrypt(encryptedHex, key, iv) {
+    var decipher = forge.cipher.createDecipher('AES-CBC', key);
+    decipher.start({iv: forge.util.hexToBytes(iv)});
+    decipher.update(forge.util.createBuffer(forge.util.hexToBytes(encryptedHex)));
+    let bunny = decipher.finish();  // if the secret key is invalid it returns false
+    if(!bunny) {
+        window.location('/secret/');
+        sweetAlert("Error", "Secret key is not valid!", "error");
+    }
+    return decipher.output.data;
+}
+
+function create() {
+
+}
