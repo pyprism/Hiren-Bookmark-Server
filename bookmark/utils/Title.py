0566ca33d0ae795548a58533c9a812853f513592
Ashutosh Das <git.pyprism@gmail.com>
2017-02-04 13:14:13 +0600

    Added parser

---
+++ bookmark/utils/Title.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+
+
+class TitleParser(HTMLParser):
+    """
+    Returns url  title !
+    """
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.match = False
+        self.title = ''
+
+    def handle_starttag(self, tag, attributes):
+        self.match = True if tag == 'title' else False
+
+    def handle_data(self, data):
+        if self.match:
+            self.title = data
+            self.match = False
