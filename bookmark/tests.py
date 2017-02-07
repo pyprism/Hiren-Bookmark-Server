d9a2e4ce17d6aade59508ea285dd3b85b158308a
Ashutosh Das <git.pyprism@gmail.com>
2017-02-07 20:12:19 +0600

    added logic view test

--- bookmark/tests.py
+++ bookmark/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from .models import Bookmark
+from . import views
+from freezegun import freeze_time
 
-# Create your tests here.
+
+class LoginViewTest(TestCase):
+    """
+    Test for login view
+    """
+    # reset_sequences = True
+
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user('hiren', 'a@b.com', 'bunny')
+
+    def test_login_url_resolves_to_login_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, views.login)
+
+    def test_auth_works(self):
+        respond = self.c.post('/', {'username': 'hiren', 'password': 'bunny'})
+        self.assertRedirects(respond, '/secret/')
+
+    def test_redirect_works_for_bad_auth(self):
+        respond = self.c.post('/', {'username': 'hiren', 'password': 'bad pass'})
+        self.assertRedirects(respond, '/')
+
+    def test_redirect_for_unauthenticated_user_works(self):
+        response = self.c.get('/dashboard/')
+        self.assertRedirects(response, '/?next=/dashboard/')
+
+    def test_authenticated_user_redirect_to_the_app(self):
+        self.c.login(username='hiren', password='bunny')
+        response = self.c.get('/', follow=True)
+        self.assertRedirects(response, '/secret/')
+
+    def test_view_returns_correct_template(self):
+        response = self.c.get('/')
+        self.assertTemplateUsed(response, 'login.html')
