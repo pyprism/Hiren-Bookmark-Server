b3c6d7d664d93cff8428057ad594046e043752b2
Ashutosh Das <git.pyprism@gmail.com>
2017-07-08 16:42:53 +0600

    added new form

--- bookmark/forms.py
+++ bookmark/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Bookmark
+from .models import Bookmark, Browser
 
 
 class BookmarkForm(ModelForm):
     class Meta:
         model = Bookmark
+        fields = '__all__'
+
+
+class BrowserForm(ModelForm):
+    class Meta:
+        model = Browser
         fields = '__all__'
