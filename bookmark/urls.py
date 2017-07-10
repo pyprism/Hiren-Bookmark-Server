f6211b992d7c024a0bc5ac7cfa3eae03b6e606d9
Ashutosh Das <git.pyprism@gmail.com>
2017-07-10 22:38:25 +0600

    added new url

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^secret/$', views.secret, name='secret'),
     url(r'^logout/$', auth.logout, {'next_page': '/'}, name="logout"),
+    url(r'^dashboard/(?P<pk>[^\.]+)/$', views.bookmark_readonly),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^dashboard_ajax/$', views.dashboard_ajax),
     url(r'^form/$', views.form, name='form'),
