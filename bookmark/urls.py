8063675207d98423dd712c200edbb39da6c6e65a
Ashutosh Das <git.pyprism@gmail.com>
2017-07-13 07:54:01 +0600

    Added new route

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^secret/$', views.secret, name='secret'),
     url(r'^logout/$', auth.logout, {'next_page': '/'}, name="logout"),
     url(r'^dashboard/(?P<pk>[^\.]+)/edit/$', views.bookmark_edit),
+    url(r'^dashboard/(?P<pk>[^\.]+)/delete/$', views.delete),
     url(r'^dashboard/(?P<pk>[^\.]+)/$', views.bookmark_readonly),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^dashboard_ajax/$', views.dashboard_ajax),
