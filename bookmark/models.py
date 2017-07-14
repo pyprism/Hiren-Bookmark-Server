82cbac4ca0fa6f5bc78a2a3aae9c451486a946fb
Ashutosh Das <git.pyprism@gmail.com>
2017-07-14 17:33:43 +0600

    deleted browser

--- bookmark/models.py
+++ bookmark/models.py
@@ -2,22 +2,7 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
-class Browser(models.Model):
-    profile_name = models.CharField(max_length=400)
-    browser_type = (
-        ('fr', 'Firefox'),
-        ('ch', 'Chrome'),
-        ('op', 'Opera'),
-        ('ot', 'Other'),
-    )
-    browser_name = models.CharField(choices=browser_type, max_length=2)
-    device_name = models.CharField(max_length=400)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
 class Bookmark(models.Model):
-    browser = models.ForeignKey(Browser, on_delete=models.CASCADE, null=True, blank=True)
     title = models.TextField()
     url = models.TextField()
     iv = models.TextField()
