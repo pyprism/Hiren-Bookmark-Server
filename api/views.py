b0f0aeaa8ad157caa897985bd94a0aba67ba6112
Ashutosh Das <git.pyprism@gmail.com>
2017-07-14 07:55:06 +0600

    Added login func

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib import auth
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def login(request):
+    """
+    token authentication for api
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            token = Token.objects.get_or_create(user=user)
+            return JsonResponse({'token': str(token[0])})
+        else:
+            return JsonResponse({'error': 'Username/Password is not valid'})
